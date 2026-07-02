ed3df4d4159fbfec0b2d186c65562f189849176a
XY-00 <xinyilee0207@gmail.com>
2026-07-02 12:14:30 +0800

    connect with flutter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation(project(":my_flutter_module"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,15 +12,19 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    // 👑 允许子模块混合私有仓库，解决 Flutter 内部加载问题
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-
-        // 👑 核心修复：把 JitPack 仓库地址引入进来，这样系统就能顺利下载 MPAndroidChart 图表库了！
+        // 确保你的图表库可以顺利下载
         maven { url = java.net.URI("https://jitpack.io") }
     }
 }
 
 rootProject.name = "myCF"
 include(":app")
+
+// 👑 标准手动申明：直接把本地的 flutter 子项目引入，不走隐式加载
+include(":my_flutter_module")
+project(":my_flutter_module").projectDir = file("../my_flutter_module")
